30924133d5e01824acffb35c67765448be1f1fb1
20100 <ahelasalah272019@gmail.com>
2024-02-20 14:15:14 +0200

    initial project

---
+++ lib/ui/auth/login/loginscreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String routeName = 'LoginScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
